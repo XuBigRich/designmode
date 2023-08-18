765b125cdd11b34ed69cd31f5456c2db4af6ccef
许鸿志 <847118663@qq.com>
2023-08-18 21:04:18 +0800

    适配器

--- src/adapter/Adapter.java
+++ src/adapter/Adapter.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * 此适配器 适配 Adaptee 类 与 Target  接口，实际上可以理解为，让Adaptee变相实现Target接口
- *
+ * 适配器要持有要适配的对象，实现要转换成目标的接口
  * @Author： hongzhi.xu
  * @Date: 2023/8/18 20:46
  * @Version 1.0
